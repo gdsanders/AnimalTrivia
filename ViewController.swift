9d057895d3e804f397992affac90a5cc48228f73
gdsanders <gd@digitalequity.com>
2015-08-05 16:46:35 -0500

    Outlets created

--- ViewController.swift
+++ ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var aLabel: UILabel!
+    @IBOutlet weak var bLabel: UILabel!
+    @IBOutlet weak var cLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
