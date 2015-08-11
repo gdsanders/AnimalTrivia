72a771b23f524bbd93054aede0eae57467a4a1ad
gdsanders <gd@digitalequity.com>
2015-08-11 16:22:55 -0500

    SecondViewController added - outlets connected

---
+++ SecondViewController.swift
@@ -0,0 +1,59 @@
+//
+//  SecondViewController.swift
+//  AnimalTrivia
+//
+//  Created by G.D. Sanders on 8/11/15.
+//  Copyright © 2015 DigitalEquity, Inc. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    @IBOutlet weak var aButton: UIButton!
+    @IBOutlet weak var incorrectAImageView: UIImageView!
+    @IBOutlet weak var aLabel: UILabel!
+    
+    
+    @IBOutlet weak var bButton: UIButton!
+    @IBOutlet weak var correctAnswerBImageView: UIImageView!
+    @IBOutlet weak var bLabel: UILabel!
+    
+    
+    @IBOutlet weak var cButton: UIButton!
+    @IBOutlet weak var incorrectAnswerCImageView: UIImageView!
+    @IBOutlet weak var cLabel: UILabel!
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func aButtonPressed(sender: UIButton) {
+        
+    }
+    
+    
+    @IBAction func bButtonPressed(sender: UIButton) {
+        
+    }
+    
+    
+    @IBAction func cButtonPressed(sender: UIButton) {
+    }
+    
+    
+    
+
+    
+}
